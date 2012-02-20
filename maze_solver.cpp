9fb385a9fb06b3c862a29ad30c15a4564deec5ca
Soo-Hyun Yoo <yoos117@gmail.com>
2012-02-20 01:56:21 -0800

    Add skeleton code for maze solver bot.

---
+++ maze_solver.cpp
@@ -0,0 +1,10 @@
+/*! \file maze_solver.cpp
+ *  \author Soo-Hyun Yoo
+ *  \brief Maze solver bot source.
+ */
+
+#include "maze_solver.h"
+
+MazeSolver::MazeSolver() {
+}
+
---
+++ maze_solver.h
@@ -0,0 +1,17 @@
+/*! \file maze_solver.h
+ *  \author Soo-Hyun Yoo
+ *  \brief Maze solver bot.
+ */
+
+#ifndef MAZE_SOLVER_H
+#define MAZE_SOLVER_H
+
+class MazeSolver {
+
+
+public
+    MazeSolver();
+}
+
+#endif // MAZE_SOLVER_H
+
