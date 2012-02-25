2782d99703a9eb41dc78a73bab00d51d65c42f17
Soo-Hyun Yoo <yoos117@gmail.com>
2012-02-25 00:53:54 -0800

    Move movement functions to separae header and source files.

--- src/maze_solver.h
+++ src/maze_solver.h
@@ -8,27 +8,7 @@
 
 #include "globals.h"
 #include "motors.h"
-
-uint64_t unitMoveStartTime;
-uint64_t unitMoveEndTime;
-
-void (*runUnitAction) ();
-
-void transUnit();
-void transForward();
-
-void veerRight();
-void veerLeft();
-
-void turnRight();
-void turnLeft();
-
-void rotateRight();
-void rotateLeft();
-void rotateBack();
-
-void stop();
-
+#include "movements.cpp"
 
 class MazeSolver {
     // Controllers
---
+++ src/movements.h
@@ -0,0 +1,30 @@
+/*! \file movements.h
+ *  \author Soo-Hyun Yoo
+ *  \brief Movements for maze solver.
+ */
+
+#ifndef MOVEMENTS_H
+#define MOVEMENTS_H
+
+uint64_t unitMoveStartTime;
+uint64_t unitMoveEndTime;
+
+void (*runUnitAction) ();
+
+void transUnit();
+void transForward();
+
+void veerRight();
+void veerLeft();
+
+void turnRight();
+void turnLeft();
+
+void rotateRight();
+void rotateLeft();
+void rotateBack();
+
+void stop();
+
+#endif // MOVEMENTS_H
+
